e8026ebe874973a88904c66d347a3bf9c6474e68
5amMickey <mgbrandt1@gmail.com>
2022-12-20 00:29:47 -0700

    Pushing the base files and folders for the rebuild

---
+++ components/app/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/login">Login</NuxtLink>
+    <NuxtLink to="/profile">Profile</NuxtLink>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <p>Login Page</p>
+  </section>
+</template>
